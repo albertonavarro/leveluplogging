044871590d91b1642bc6dc20994aea00bc581669
Alberto Navarro <alberto.cartagena@gmail.com>
2018-09-03 22:07:02 +0200

    Initial commit

---
+++ src/main/java/Main1.java
@@ -0,0 +1,14 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main1 {
+
+    private static final Logger logger = LoggerFactory.getLogger(Main1.class);
+
+    public static void main(String[] args) {
+
+        logger.info("Starting main - without arguments");
+
+    }
+
+}
