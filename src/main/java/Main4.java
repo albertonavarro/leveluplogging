7bfe606f3fcdce61fa95d705651e29c7ecf9b2b7
Alberto Navarro <alberto.cartagena@gmail.com>
2018-09-06 00:41:35 +0200

    more examples added

--- src/main/java/Main4.java
+++ src/main/java/Main4.java
@@ -1,5 +1,8 @@
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+
+import java.util.UUID;
 
 import static net.logstash.logback.argument.StructuredArguments.kv;
 
@@ -8,9 +11,9 @@ public class Main4 {
     private static final Logger logger = LoggerFactory.getLogger(Main4.class);
 
     public static void main(String[] args) {
-
+        MDC.put("rid", UUID.randomUUID().toString());
         logger.info("New Market Status {} {}", kv("marketId", 1234), kv("newStatus", "ready"));
-
+        logger.info("Some more logging");
     }
 
 }
